ebe3c5a2c61c38724cd5b800662b84a2f71081bd
Karn Wong <karn@karnwong.me>
2025-04-03 19:57:30 +0700

    disable mock resource

--- staging/main.tf
+++ staging/main.tf
@@ -9,11 +9,11 @@ resource "random_uuid" "uuid" {
 resource "random_uuid" "uuid2" {
 }
 
-
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["foo"]
-  subnets            = ["bar"]
-}
+#
+# resource "aws_lb" "test" {
+#   name               = "test-lb-tf"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = ["foo"]
+#   subnets            = ["bar"]
+# }
